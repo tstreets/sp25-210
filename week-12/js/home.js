ab0d97fd1dedb0cc2aea5cb4b1862ed63d442c5c
Ty the Fly Guy <tstreets@iu.edu>
2025-04-01 11:46:12 -0400

    Showing dreams

--- week-12/js/home.js
+++ week-12/js/home.js
@@ -10,9 +10,19 @@ const db = getFirestore(app);
 
 const dreamsCollection = collection(db, "hopesdreams");
 
+const dreamsRef = document.querySelector("#dreams");
+
 async function getDreams() {
   const dreamsDocs = await getDocs(dreamsCollection);
-  console.log(dreamsDocs.docs.length);
+
+  dreamsRef.innerHTML = "";
+  for (let i = 0; i < dreamsDocs.docs.length; i++) {
+    const currentDream = dreamsDocs.docs[i];
+    console.log(currentDream.id, currentDream.data());
+
+    const dreamData = currentDream.data();
+    dreamsRef.innerHTML += "<li>" + dreamData.text + "</li>";
+  }
 }
 
 getDreams();
